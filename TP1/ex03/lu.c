1dae42f96e2b05726c73dcf531c19d2b0ff79d9f
Meriem Elkhal <65033615+sprixie@users.noreply.github.com>
2020-12-17 19:03:28 +0100

    Update lu.c

--- TP1/ex03/lu.c
+++ TP1/ex03/lu.c
@@ -5,18 +5,47 @@
 float *lu(float A[N][N], float B[N], int n)
 {
 
-  /******Implement the solution here******/
-  /***************************************/
-
-
-  /***************************************/
-  return (x);
+  float  *X , L[N][N] , Y[N] ;
+  X = (float *)malloc(sizeof(float));
+  /*Gauss elimination on A to get L and U*/
+  for(int k = 0 ; k < n-1 ; k++)
+  {
+    for(int i = k+1 ; i < n ; i++) 
+    {
+      L[i][k] = A[i][k] / A[k][k];
+      for(int j = k + 1; j < n; j++)
+      {
+        A[i][j] = A[i][j]- L[i][k] * A[k][j];
+      }
+    }
+  }
+  /*calculating Y with LY=B (forward substitution)*/
+  Y[0] = B[0];
+  for(int i = 1 ; i < n ; i++)
+  {
+    int s = 0;
+    for(int j = 0 ; j < i ; j++)
+    {
+      s= s + L[i][j] * Y[j];
+    }
+    Y[i] = B[i] - s;
+  }
+  /*calculating X with UX=Y (back substitution)*/
+  X[n-1] = Y[n-1] / A[n-1][n-1];
+  for(int i = n-2 ; i >= 0 ; i--)
+  {
+    float s = 0;
+    for(int j = i + 1 ; j <= n-1 ; j++)
+      s += A[i][j] * X[j];
+    X[i] = (Y[i] - s)/A[i][i];
+  }      
+  return (X);
 }
 
 int main()
 {
   float   A[N][N], B[N];
-  float   *x;
+  float   *X;
   int     n;
 
   printf("Enter the size of the matrix: ");
@@ -42,11 +71,11 @@ int main()
   }
 
   /* The calculation of the result */
-  x = lu(A, B, n);
+  X = lu(A, B, n);
 
   /* Printing the results */
   printf("\nThe resulting vector: [");
   for (int i = 0; i < n; i++)
-    printf("%f%c", x[i], ",]"[i == n - 1]);
+    printf("%f%c", X[i], ",]"[i == n - 1]);
 }
 
